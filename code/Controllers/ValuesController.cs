1571ecf64b0b3a2da2e0a079040ab75fd91449a8
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-08-13 19:07:53 +0200

    added readme.md

---
+++ code/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace creating_a_aspnet_core_webapit_with_the_dotnet_cli.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var customerOne = new Customer()
+            {
+                Name = "Phil",
+                Age = 60
+            };
+            var customerTwo = new Customer()
+            {
+                Name = "Mike",
+                Age = 61
+            };
+            return Ok(new List<Customer>() { customerOne, customerTwo });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]string value)
+        {
+            return CreatedAtAction(nameof(Get), value);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+Repository for my egghead course
+
+## Create an ASP.NET Core WebAPI with the dotnet cli and Visual Studio Code
+
+[Create an ASP.NET Core WebAPI with the dotnet cli and Visual Studio Code](https://egghead.io/lessons/angular-create-an-asp-net-core-webapi-with-the-dotnet-cli-and-visual-studio-code)
+
+![instructor-medium-5@2x.png](.github/instructor-medium-5@2x.png)
